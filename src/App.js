8510abca06ebac19ab8c77cbdf0e63f3a1de0ef3
mudassirarif17 <mudassirinoxent@gmail.com>
2023-09-27 12:26:47 +0500

    understanding about button in mui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 function App() {
   return (
     <div>
+      {/*1. Typography */}
       <Typography variant='h1' sx={{color : "red" , background : "blue"}} component={"h2"} mt={20}>Aptech Garden</Typography>
       <h1>MUI Tutorial</h1>
-      <p>Pakistan is my country . I love pakistan </p> 
+      {/* <p>Pakistan is my country . I love pakistan </p>  */}
+
+      {/*2. Buttons in MUi */}
+      {/* Three types of Buttons text is bydefault */}
+      <Button variant='text' sx={{margin : "30px"}}>Click Me</Button>
+      <Button variant='contained' color='success' disabled size='large'>Click Me</Button>
+      <Button onClick={()=> alert("You clicked the button")} variant='outlined'>Click Me for action</Button>
     </div>
   );
 }
